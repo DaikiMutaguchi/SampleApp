b4dd192dc2d60434a52bdb7129f798ee00301392
DaikiMutaguchi <mucchi43@outlook.jp>
2020-12-16 12:56:11 +0900

    Initial Commit

---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Daiki Mutaguchi on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

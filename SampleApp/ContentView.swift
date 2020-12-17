9b7ed1d81e7fb19d9f2cdfd643413ca157682ae7
NikoNiko72 <75941039+NikoNiko72@users.noreply.github.com>
2020-12-17 13:02:04 +0900

    ひらがなにしたよ

--- SampleApp/ContentView.swift
+++ SampleApp/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 .tabItem {
                     VStack {
                         Image(systemName: "eyes")
-                        Text("お化け図鑑")
+                        Text("おばけ図鑑")
                     }
             }.tag(2)
             

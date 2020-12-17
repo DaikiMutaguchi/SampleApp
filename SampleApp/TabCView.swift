4d39b0112671055d5a6a8e0714c83de1d7b9699d
DaikiMutaguchi <mucchi43@outlook.jp>
2020-12-18 02:30:16 +0900

    テキストフィールド追加

--- SampleApp/TabCView.swift
+++ SampleApp/TabCView.swift
@@ -24,6 +24,7 @@ struct TabCView: View {
                     
                    }
                     
+                    
                     ZStack {
                         Image("TbBG")
                             .resizable()

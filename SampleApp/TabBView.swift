2b9a05d4be0a1038b971a7e651cfd55619f8e730
DaikiMutaguchi <mucchi43@outlook.jp>
2020-12-18 02:28:56 +0900

    テキストエリア追加

--- SampleApp/TabBView.swift
+++ SampleApp/TabBView.swift
@@ -20,3 +20,5 @@ struct TabBView_Previews: PreviewProvider {
         ObakeView()
     }
 }
+
+
--- SampleApp/TabCView.swift
+++ SampleApp/TabCView.swift
@@ -8,16 +8,18 @@
 import SwiftUI
 
 struct TabCView: View {
+    
     var body: some View {
-           NavigationView {
-               NavigationLink(destination: TabAView()) {
+           NavigationView {VStack {
+                    
+               NavigationLink(destination: TBDataView()) {
                 
                 HStack{
                    ZStack {
                        Image("TbBG")
                            .resizable()
                            .frame(width: 70, height: 130)
-                        Text("演習")
+                        Text("システム論")
                     
                     
                    }
@@ -42,5 +44,54 @@ struct TabCView: View {
 struct TabCView_Previews: PreviewProvider {
     static var previews: some View {
         TabCView()
+        }
     }
 }
+
+
+
+struct TBDataView: View {
+    
+    @State var name : String = ""
+    @State var message : String = ""
+    @State var editting = false
+    
+    
+    var body: some View {
+        VStack {
+            TextField("名前を入力して下さい", text: $name,
+                      
+                onEditingChanged: { begin in
+                    /// 入力開始処理
+                    if begin {
+                        self.editting = true    // 編集フラグをオン
+                        self.message = ""       // メッセージをクリア
+                            
+                        /// 入力終了処理
+                    } else {
+                        self.editting = false   // 編集フラグをオフ
+                    }
+                },
+ 
+                /// リターンキーが押された時の処理
+                onCommit: {
+                    self.message = "月曜日１限：\(self.name)"   // メッセージセット
+                    self.name = ""  // 入力域をクリア
+                    
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle()) // 入力域を枠で囲む
+                .padding()      // 余白を追加
+                // 編集フラグがONの時に枠に影を付ける
+                .shadow(color: editting ? .blue : .clear, radius: 3)
+            
+            Text(verbatim: message)
+        }
+    }
+}
+
+
+struct TBDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        TBDataView()
+        }
+    }

de09a4a3cb9a2748f3ac39c9e48165fab632e2ed
NikoNiko72 <75941039+NikoNiko72@users.noreply.github.com>
2020-12-19 18:15:35 +0900

    ちょこまか

--- SampleApp/TabAView.swift
+++ SampleApp/TabAView.swift
@@ -8,23 +8,26 @@
 import SwiftUI
 
 struct TabAView: View {
-    @State private var todo = "" //ユーザー入力
+    @State private var toDo = "" //ユーザー入力
     @State private var buttonLabel = "" //ボタンに表示する
     @State private var inputValue = "" //
     var body: some View {
         
         NavigationView { //ナビゲーションビュー
-            List(1..<3) { index in
+            List(1..<2) { index in
                             NavigationLink(destination:
-                                         Text("遷移先画面")){ Text("\(index)行目")
+                                        Text("遷移先画面")){ Text("\(index)行目")
                                         }
+                
+                
                 VStack { //縦に並べます
-                           TextField("ToDoを追加する", text: $todo) //テキストフィールド設定
+                           TextField("ToDoを追加する", text: $toDo) //テキストフィールド設定
                                .textFieldStyle(RoundedBorderTextFieldStyle())  // 入力域のまわりを枠で囲む
                                .padding()  // 余白を追加
-                    
-                       }
-                        }.navigationTitle("ToDoリスト")//タイトル
+                }
+            }.navigationTitle("ToDoリスト")//タイトル
+            
+            
             }
         }
     }

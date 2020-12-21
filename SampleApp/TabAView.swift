684cb99967f07ee496bca06a0dee4fc79bb93b01
NikoNiko72 <75941039+NikoNiko72@users.noreply.github.com>
2020-12-21 21:27:38 +0900

    ナビゲーションバーにナビゲーションリンク貼りたいけどエラー出まくってる

--- SampleApp/TabAView.swift
+++ SampleApp/TabAView.swift
@@ -8,55 +8,38 @@
 import SwiftUI
 
 struct TabAView: View {
-    @State private var toDo = "" //ユーザー入力
-    @State private var buttonLabel = "" //ボタンに表示する
-    @State private var message = "ここをリスト表示したい" //
-    var body: some View {
-        
-        NavigationView { //ナビゲーションビュー
-            List(1..<6) { index in
-                            NavigationLink(destination:
-                                        Text("遷移先画面")){ Text("\(index)行目")
-                                        }
-                Text(message)
-                .navigationBarTitle("ToDoリスト", displayMode: .automatic)
-                    .navigationBarItems(trailing:
-                        Button(action: {
-                            self.message = "ボタンは機能してます"
-                        }) {
-                                    Image(systemName: "plus")
-                        }
-                    )
-            }
-        }
-        
-        
-        
-        }
+       @State var messages: [String] = []
+       @State var newMessage = ""
+       var body: some View {
+           VStack(alignment: .leading) {
+               VStack(alignment: .leading) {
+                   Text("リスト一覧にToDoを追加")
+                       .font(.footnote)
+                       .foregroundColor(.gray)
+                   HStack {
+                       TextField("ToDoを入力", text: $newMessage).textFieldStyle(RoundedBorderTextFieldStyle())
+                       Button(action: {
+                           self.messages.append(self.newMessage)
+                           self.newMessage = ""
+                       }) {
+                           Text("Done")
+                               .buttonStyle(BorderlessButtonStyle())
+                       }
+                   }
+               }.padding([.leading, .trailing])
+               
+               List {
+                   ForEach(messages, id: \.self) { user in
+                       Text(user)
+                   }
+               }
+           }
+       }
     }
-
-
 struct TabAView_Previews: PreviewProvider {
     static var previews: some View {
         TabAView()
     }
 }
-
-
-
-struct ListInput {
-    @State private var memo = "仮" //
-    var body: some View {
-        NavigationView { //ナビゲーションビュー
-            Text(memo)
-            .navigationBarTitle("ToDoリスト", displayMode: .automatic)
-
-        }
-        
-
-        
-    }
-}
-
 
 

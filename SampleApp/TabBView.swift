0546e6411f560cd96e6a5a90e4d0e02172d4e448
NikoNiko72 <75941039+NikoNiko72@users.noreply.github.com>
2021-01-10 21:04:44 +0900

    多分一卍最終コミット

--- SampleApp/TabBView.swift
+++ SampleApp/TabBView.swift
@@ -13,12 +13,11 @@ struct TabBView: View {
             .resizable()//画像をフレームの大きさに合わせる
             .frame(width: 380.0, height: 700.0, alignment: .leading)
                     //フレームサイズの指定
-    
     }
 }
 
 struct TabBView_Previews: PreviewProvider {
     static var previews: some View {
         TabBView()
-    }
+    }//TabBViewシミュレーター
 }

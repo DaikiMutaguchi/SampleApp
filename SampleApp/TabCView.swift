03f7fb56089f8c9888f3e3f14a896c8b99f15778
DaikiMutaguchi <75073931+DaikiMutaguchi@users.noreply.github.com>
2021-02-27 03:27:28 +0900

    テキスト表示ゾーン編集

--- SampleApp/TabCView.swift
+++ SampleApp/TabCView.swift
@@ -51,8 +51,13 @@ struct TabCView: View {
                                         .frame(width: 65, height: 100)
                                 VStack{
                                 Text(CSubTitle.SubTitle[Date*5+time])
-                                Spacer()
+                                        .background(Color.gray)    // Text Viewの背景を青に
+                                        .frame(width: 65, height:80 , alignment: .top)  // フレームの設定
+                                        .border(Color.red, width: 2)   // フレームに枠線をつけて可視化
+                                    
                                 Text(CClassNo.ClassNo[Date*5+time])
+                                    .frame(width: 65, height:20 ,alignment: .bottom)  // フレームの設定
+                                    .border(Color.green, width: 2)
                                 }
                                 
                             }

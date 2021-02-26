9094fe44a3a5dbbc4c476c0842a813dee70373f5
DaikiMutaguchi <75073931+DaikiMutaguchi@users.noreply.github.com>
2021-02-26 21:21:48 +0900

    編集画面いい感じじゃない？

---
+++ SampleApp/TableEditView.swift
@@ -0,0 +1,139 @@
+//
+//  TableEditView.swift
+//  SampleApp
+//
+//  Created by 牟田口大輝 on 2021/02/26.
+//
+
+import SwiftUI
+
+
+
+struct TableEditView: View {
+    let Date = ["月曜日", "火曜日", "水曜日", "木曜日", "金曜日"]
+    let Jikan = ["１限","２限","３限","４限","５限"]
+    @State private var Dselection = 0
+    @State private var Jselection = 0
+    
+    
+    @State var subname : String = ""
+    @State var submessage : String = ""
+    @State var subeditting = false
+    
+    @State var clname : String = ""
+    @State var clmessage : String = ""
+    @State var cleditting = false
+    
+    
+    
+    var body: some View {
+        NavigationView {    // Formで使う場合はNavigationViewが必須
+            
+            
+            Form {
+                Picker(selection: $Dselection, label: Text("曜日")) {
+                    ForEach(0 ..< Date.count) { num1 in
+                        Text(self.Date[num1])
+                    }
+                }
+                
+                
+                Picker(selection: $Jselection, label: Text("時間")) {
+                    ForEach(0 ..< Jikan.count) { num2 in
+                        Text(self.Jikan[num2])
+                    }
+                }
+                
+                HStack{
+                Text("科目")//①
+                    Spacer()
+                    Text(verbatim: submessage)
+                }
+                    TextField("科目名を入力して下さい", text: $subname,//③
+                              
+                        onEditingChanged: { begin in//編集中に関する処理開始
+                            //入力開始処理
+                            if begin {
+                                self.subeditting = true    // 編集フラグをオン
+                                self.submessage = ""       // メッセージをクリア
+                                //入力終了処理
+                            }
+                            
+                            else {
+                                self.subeditting = false   // 編集フラグをオフ
+                            }
+                        },//編集中に関する処理終了
+         
+                        
+                        onCommit: {//リターンキーが押された時の処理
+                            self.submessage = "\(self.subname)"   // メッセージセット
+                            self.subname = ""  // 入力域をクリア
+                        }
+                        //リターンキー処理終了
+                        
+                    )//テキストフィールド終了
+                    
+                    //テキストフィールドに関する設定
+                        .textFieldStyle(RoundedBorderTextFieldStyle()) // 入力域を枠で囲む
+                        
+                        // 編集フラグがONの時に枠に影を付ける
+                        .shadow(color: subeditting ? .blue : .clear, radius: 3)
+                    //設定終了
+                    
+        
+                
+                HStack{
+                    Text("教室")
+                    Spacer()
+                    Text(verbatim: clmessage)//②
+                    
+                }
+                    TextField("教室を入力して下さい", text: $clname,
+                              
+                        onEditingChanged: { begin in
+                            
+                            if begin {
+                                self.cleditting = true
+                                self.clmessage = ""
+                                    
+                                
+                            }
+                            else {
+                                self.cleditting = false
+                            }
+                        },
+         
+                        
+                        
+                        onCommit: {
+                            self.clmessage = "\(self.clname)"
+                            self.clname = ""
+                            
+                        }
+                    
+                    )//テキストフィールド終了
+                    
+                    //テキストフィールドに関する設定
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        
+                        .shadow(color: cleditting ? .blue : .clear, radius: 3)
+                    //設定終了
+                
+                
+                
+                
+            }
+            
+            
+            
+           
+            .navigationBarTitle("時間割の編集")
+        }
+    }
+}
+
+struct TableEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        TableEditView()
+    }
+}

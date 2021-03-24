b65611143df331eff416ac83bf0f8235370cc37e
NikoNiko72 <75941039+NikoNiko72@users.noreply.github.com>
2021-03-24 15:43:46 +0900

    変数名をキャメルケースに

--- SampleApp/TabAView.swift
+++ SampleApp/TabAView.swift
@@ -8,54 +8,60 @@
 import SwiftUI
 
 struct TabAView: View {
-       @State var alist: [String] = []//リストを並べる
-       @State var newalist = ""   //ユーザーに入力してもらう
+       @State var Alist: [String] = []//リストを並べる
+       @State var NewAlist = ""   //ユーザーに入力してもらう
+        @State var NewPoint = "" //タスク完了で得られるポイント
+        @State var SavePoint = "" //ポイント累積
        var body: some View {
         //bodyの開始
-        
+
         NavigationView{
-           VStack(alignment: .leading) { //下部の要素とタスク表示部を縦に並べる
-            
-               VStack(alignment: .leading) {//下部の要素とテキストを縦に並べる
-                   Text("課題をリストに追加しよう！")
+
+            VStack(alignment: .leading) { //操作部とリストを縦に並べる
+
+                VStack(alignment: .leading) {//テキストとその下の要素を縦に並べる
+                    Text("課題をリストに追加しよう！")
                        .font(.footnote)
                        .foregroundColor(.gray)
                 
-                   HStack {//下部の要素とテキスト入力フィールドを横に並べる
-                       TextField("ToDoを入力", text: $newalist)
+                    HStack {//入力フィールドとDoneボタンを横に並べる
+                       TextField("ToDoを入力", text: $NewAlist) //入力タスクをフィールドに
                         .textFieldStyle(RoundedBorderTextFieldStyle()) //入力フィールドを線で囲む
                     
-                       Button(action: { //タスク登録ボタン
-                           self.alist.append(self.newalist)//配列に要素を追加
-                           self.newalist = ""//入力フィールドを空に
+                        Button(action: { //Doneボタン
+                           self.Alist.append(self.NewAlist)//配列に要素を追加
+                           self.NewAlist = ""//入力フィールドを空に
                        }) {
                            Text("Done")//ボタンのスタイル
                                .buttonStyle(BorderlessButtonStyle())
-                       }.disabled(newalist.isEmpty)  //文字が入力されてない時は押せない
+                       }.disabled(NewAlist.isEmpty)  //文字が入力されてない時は押せない
                     
-                   }//入力フィールドとボタンの横並び終了
+                   }//入力フィールドとDoneボタンの横並び終了
                                 
                }.padding([.leading, .trailing])
-                //テキストと横並び要素の縦並び終了、要素の両端にスペースを設けて配置
-               
+                //テキストからDoneボタンまでの配置終了、両端にスペースを設ける
+
                List {//リスト表示
-                   ForEach(alist, id: \.self) { user in
+                   ForEach(Alist, id: \.self) { user in
                        Text(user)
                    }
                    .onDelete(perform: self.deleteRow) // 削除処理呼び出し
                }//リスト表示終了
            }//リストと要素の縦並び終了
-           .navigationTitle("ToDoリスト")
             
+           .navigationTitle("ToDoリスト")
             }//ナビゲーションビューの終了
        }//bodyの終了
+    
         func deleteRow(offsets: IndexSet) {
             //リスト削除処理
-                self.alist.remove(atOffsets: offsets)
+                self.Alist.remove(atOffsets: offsets)
             //スライドして削除
             
             }
     }//TabAView終了
+
+
 
 struct TabAView_Previews: PreviewProvider {//TabAViewのシミュレーター
     static var previews: some View {

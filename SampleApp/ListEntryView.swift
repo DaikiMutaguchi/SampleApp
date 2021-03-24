b683109da1df75fb6c121e16fd3dbcc981bc4235
NikoNiko72 <75941039+NikoNiko72@users.noreply.github.com>
2021-03-24 15:59:13 +0900

    リスト登録用のファイル作成

---
+++ SampleApp/ListEntryView.swift
@@ -0,0 +1,20 @@
+//
+//  ListEntryView.swift
+//  SampleApp
+//
+//  Created by kana on 2021/03/24.
+//
+
+import SwiftUI
+
+struct ListEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListEntryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListEntryView()
+    }
+}

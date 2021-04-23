a7d5c27e2d3405085f647416a823fecb833d3e9a
DaikiMutaguchi <mucchi43@outlook.jp>
2021-04-23 14:54:47 +0900

    得点の配列設定

--- SampleApp/TabBView.swift
+++ SampleApp/TabBView.swift
@@ -8,35 +8,33 @@
 import SwiftUI
 
 struct TabBView: View {
+    let GetPoint = [1,3,5,10,15,20,25,30,40,50,60,70,80,90,100,150,200,300,400,500,]
+    
     @EnvironmentObject var TaskPoint: SjAndCn //タスク完了で得られるポイント
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             
         ZStack{
             Image("TbBG")
-        VStack{
-            Text("今は\(TaskPoint.Point)")
+                .resizable()
+                .frame(width: 400, height: 800)
         
-            if self.TaskPoint.Point >= 0{
-                Image("1E93370C-DA49-4A0A-A3EE-0DD87567F2B5")
-                    .resizable()
-                    .frame(width: 400, height: 300)
+            
+            ForEach((0...19), id: \.self) { Loop in
+                    
+             
+                if self.TaskPoint.Point >= GetPoint[Loop]{
+                    Image("1E93370C-DA49-4A0A-A3EE-0DD87567F2B5")
+                        .resizable()
+                        .frame(width: 400, height: 300)
+                }
+            
+                
+                
+            
             }
+        
             
-            if self.TaskPoint.Point >= 1{
-                Image("9923C1AB-D9F8-471B-9E8E-801677D4E3B0")
-                    .resizable()
-                    .frame(width: 400, height: 300)
-            }
-            
-            if self.TaskPoint.Point >= 2{
-                Image("9923C1AB-D9F8-471B-9E8E-801677D4E3B0")
-                    .resizable()
-                    .frame(width: 400, height: 300)
-            }
-            
-            
-        }
         }
         }
     }

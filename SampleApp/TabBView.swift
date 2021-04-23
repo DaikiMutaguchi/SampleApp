84216cadad58f215824afb1e8cdfe106b5d47664
DaikiMutaguchi <75073931+DaikiMutaguchi@users.noreply.github.com>
2021-04-23 11:23:59 +0900

    条件判断で画像の出現

--- SampleApp/TabBView.swift
+++ SampleApp/TabBView.swift
@@ -10,16 +10,36 @@ import SwiftUI
 struct TabBView: View {
     @EnvironmentObject var TaskPoint: SjAndCn //タスク完了で得られるポイント
     var body: some View {
-    
-        HStack{
-            Text("テキスト")
+        ScrollView(.vertical, showsIndicators: false) {
+            
+        ZStack{
+            Image("TbBG")
+        VStack{
+            Text("今は\(TaskPoint.Point)")
         
-            if self.TaskPoint.Point >= 2{
-                Image(systemName: "minus.circle")
+            if self.TaskPoint.Point >= 0{
+                Image("1E93370C-DA49-4A0A-A3EE-0DD87567F2B5")
+                    .resizable()
+                    .frame(width: 400, height: 300)
             }
+            
+            if self.TaskPoint.Point >= 1{
+                Image("9923C1AB-D9F8-471B-9E8E-801677D4E3B0")
+                    .resizable()
+                    .frame(width: 400, height: 300)
+            }
+            
+            if self.TaskPoint.Point >= 2{
+                Image("9923C1AB-D9F8-471B-9E8E-801677D4E3B0")
+                    .resizable()
+                    .frame(width: 400, height: 300)
+            }
+            
+            
         }
         }
-    
+        }
+    }
 }
 
 struct TabBView_Previews: PreviewProvider {

8b53272d2ade04c105a43ba0d40de7c3eb7e0bbd
DaikiMutaguchi <75073931+DaikiMutaguchi@users.noreply.github.com>
2021-06-24 05:27:14 +0900

    起動画面の持続時間変更（？）

--- SampleApp/SampleAppApp.swift
+++ SampleApp/SampleAppApp.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 @main
-struct SampleAppApp: App {
+struct SwiftUIAppSample: App {
+    // swiftlint:disable weak_delegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate  // 追加する
     var body: some Scene {
-        
         WindowGroup {
             ContentView()
         }
     }
+}
+
+// 以下を追加する
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        sleep(2)
+        return true
+        
+    }
+    
 }
